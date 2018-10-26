82f82ddc3a4cf1f331f196b0dc0e0f293b50c9e0
test <476159054@qq.com>
2018-10-26 20:03:10 +0800

    test

---
+++ mysite/polls/templates/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+# @Time    : 2018/10/25 0025 10:07
+# @Author  : LiYong
+# @Site    : 
+# @File    : __init__.py.py
---
+++ mysite/polls/templates/polls/__init__.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+# @Time    : 2018/10/25 0025 10:09
+# @Author  : LiYong
+# @Site    : 
+# @File    : __init__.py.py
